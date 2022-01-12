d5835e63010e23d36fe07e342f6bc622e0aa3e10
Srinivasan Arumugham <srinivasan2003@outlook.com>
2022-01-12 00:37:16 -0800

    fix

---
+++ Bronze/1083/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    string h;
+    cin >> a >> h;
+    int arr[h.size()];
+
+    for (int i = 0; i < h.size(); i++) {
+        arr[i] = a.find(h[i]);
+    }
+
+    int m = arr[0];
+    int n = 0;
+
+    for (int i : arr) {
+        if (i <= m) {
+            m = i;
+            n++;
+        }
+        else {
+            m = i;
+        }
+    }
+
+    cout << n;
+    return 0;
+}
